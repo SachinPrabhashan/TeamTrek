b9c4edfd7615907a56b9c5cdb85aadcdca6eb3f1
SachinPrabhashan <sachinprabhashan@gmail.com>
2024-02-27 11:21:03 +0530

    Initial Root User Seeder File

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    public function run()
+    {
+        // Create the root user
+        DB::table('users')->insert([
+            'name' => 'TeamTrek-ROOT',
+            'email' => 'teamtrek@root.com',
+            'password' => Hash::make('12345678'),
+            // Add any other necessary fields
+        ]);
+    }
+}
