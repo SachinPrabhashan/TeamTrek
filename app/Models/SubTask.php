66c662a26e899f1ff62fd9b9b7c2589b5256c64c
Your Name <you@example.com>
2024-03-15 12:17:24 +0530

    User Id for the Task & Sub Tasks

--- app/Models/SubTask.php
+++ app/Models/SubTask.php
@@ -10,6 +10,7 @@ class SubTask extends Model
     use HasFactory;
 
     protected $fillable = [
+        'user_id',
         'support_contract_instance_id',
         'name',
         'date',
--- database/migrations/2024_03_14_101422_create_sub_tasks_table.php
+++ database/migrations/2024_03_14_101422_create_sub_tasks_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('sub_tasks', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('task_id');
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
             $table->string('name');
             $table->date('date');
@@ -21,6 +22,7 @@ return new class extends Migration
             $table->integer('eng_hours');
 
             $table->foreign('task_id')->references('id')->on('tasks');
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
